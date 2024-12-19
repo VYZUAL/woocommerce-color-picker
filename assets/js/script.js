77267af3c62dd4c12d33f1f53283d6f2c77851b1
VYZUAL <118173370+VYZUAL@users.noreply.github.com>
2024-12-19 16:09:01 +0100

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,41 @@
 jQuery(document).ready(function ($) {
+    // Selecteer relevante elementen
     const circles = $('.color-circle');
     const selectInput = $('select[name="attribute_pa_kleur"]');
+    const resetButton = $('.elementor-widget-woocommerce-product-add-to-cart .reset_variations');
     const label = $('<div id="selected-color-label"></div>');
 
     if (selectInput.length) {
+        // Verberg dropdown en voeg tekstlabel toe
         selectInput.hide().after(label);
     }
 
+    // Verberg standaard WooCommerce-labels en "Wissen"-knop
+    $('label[for="attribute_pa_kleur"]').hide();
+    resetButton.hide();
+
+    // Kleurselectie via bolletjes
     circles.on('click', function () {
-        const value = $(this).data('value');
-        selectInput.val(value).trigger('change');
-        label.text('Selected color: ' + value);
+        const value = $(this).data('value'); // Haal de waarde (slug) van het bolletje op
+        selectInput.val(value).trigger('change'); // Stel de waarde in voor WooCommerce
+
+        // Zoek de naam van de geselecteerde optie in de dropdown
+        const optionText = selectInput.find(`option[value="${value}"]`).text();
+        label.text(optionText); // Toon de termnaam als label
+
+        // Highlight het geselecteerde bolletje
         circles.removeClass('selected');
         $(this).addClass('selected');
+
+        // Toon de "Wissen"-knop
+        resetButton.show();
+    });
+
+    // Verberg "Wissen"-knop bij reset
+    resetButton.on('click', function () {
+        label.text(''); // Reset het label
+        circles.removeClass('selected');
+        selectInput.val('').trigger('change'); // Reset de waarde
+        resetButton.hide();
     });
 });
